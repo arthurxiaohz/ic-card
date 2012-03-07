e562438f7f48f8f1be09e74f56f48151f5a4703f
Angi.Wang.AFE <Angi.Wang.AFE@gmail.com>
2012-03-07 07:33:47 +0000

    batch

---
+++ BM/src/cn/net/iccard/batch/InitiativeDistributeCoupon.java
@@ -0,0 +1,19 @@
+package cn.net.iccard.batch;
+
+import org.hi.base.schedule.quartz.JobDetail;
+
+/**
+ * 主动发起红包到会员
+ * 
+ * @author Angi
+ * 
+ */
+public class InitiativeDistributeCoupon extends JobDetail {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
