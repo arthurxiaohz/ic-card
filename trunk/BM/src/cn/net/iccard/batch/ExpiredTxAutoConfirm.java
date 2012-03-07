bf5a93207d7ad8f65b55528a27ebe52821e4db10
Angi.Wang.AFE@gmail.com <Angi.Wang.AFE@gmail.com@3782fa02-7c17-0661-8314-7abf71340a32>
2012-03-07 07:33:47 +0000

    batch

---
+++ trunk/BM/src/cn/net/iccard/batch/ExpiredTxAutoConfirm.java
@@ -0,0 +1,19 @@
+package cn.net.iccard.batch;
+
+import org.hi.base.schedule.quartz.JobDetail;
+
+/**
+ * 到期交易自动确认支付
+ * 
+ * @author Angi
+ * 
+ */
+public class ExpiredTxAutoConfirm extends JobDetail {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
